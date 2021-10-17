d09a343a41dfd0be76187eb062a62ee9eeeb6d18
Ipsky <yap@usc.edu>
2021-10-17 14:16:59 -0700

    define a combine publisher as well as an async task

--- Sources/Twig/Timeline.swift
+++ Sources/Twig/Timeline.swift
@@ -6,12 +6,29 @@
 //
 
 import Foundation
+import Combine
 
+/// `async` approach.
 public func timeline(credentials: OAuthCredentials) async throws -> [RawTweet] {
     let request = timelineRequest(credentials: credentials)
     let (data, _): (Data, URLResponse) = try await URLSession.shared.data(for: request, delegate: nil)
-    
-    let failables = try! JSONDecoder().decode([Failable<RawTweet>].self, from: data)
+    return try decodeTweets(from: data)
+}
+
+/// Combine approach.
+public func timelinePublisher(credentials: OAuthCredentials) -> AnyPublisher<[RawTweet], Error> {
+    let request = timelineRequest(credentials: credentials)
+    return URLSession.shared.dataTaskPublisher(for: request)
+        .tryMap { (data, _) in try decodeTweets(from: data) }
+        .eraseToAnyPublisher()
+}
+
+// MARK: - Guts
+/// Decode JSON array of Tweets.
+/// If there are malformed values in the array,
+/// salvage the good ones, and cause a dev crash for the bad ones.
+internal func decodeTweets(from data: Data) throws -> [RawTweet] {
+    let failables = try JSONDecoder().decode([Failable<RawTweet>].self, from: data)
     return failables.compactMap { (failable) -> RawTweet? in
         if let tweet = failable.item {
             return tweet
