f76c3714d7b675b5d8f5af7bf45efb5484238396
Ipsky <yap@usc.edu>
2021-10-23 15:29:14 -0700

    move function

--- Sources/Twig/Timeline.swift
+++ Sources/Twig/Timeline.swift
@@ -24,27 +24,6 @@ public func timelinePublisher(credentials: OAuthCredentials) -> AnyPublisher<[Ra
 }
 
 // MARK: - Guts
-/// Decode JSON array.
-/// If there are malformed values in the array,
-/// salvage the good ones, and cause a dev crash for the bad ones.
-internal func decodeFailableArray<T: Decodable>(from data: Data) throws -> [T] {
-    /// Attempt to decode each item, replacing failures with `nil`.
-    let failables = try JSONDecoder().decode([Failable<T>].self, from: data)
-    
-    /// Filter out `nil` values in production.
-    return failables.compactMap { (failable) -> T? in
-        if let item = failable.item {
-            return item
-        } else {
-            #if DEBUG
-            /// Intentionally crash to reveal error.
-            _ = try! JSONDecoder().decode([T].self, from: data)
-            #endif
-            return nil
-        }
-    }
-}
-
 internal func timelineRequest(credentials: OAuthCredentials) -> URLRequest {
     var timelineURL = "https://api.twitter.com/1.1/statuses/home_timeline.json"
     
