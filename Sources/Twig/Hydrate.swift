b12ce44c5c29d812d6fcd1f9d5a3d78528d6f502
Ipsky <yap@usc.edu>
2021-10-23 15:09:06 -0700

    define enum

--- Sources/Twig/Hydrate.swift
+++ Sources/Twig/Hydrate.swift
@@ -30,6 +30,19 @@ public enum TweetField: String {
     case withheld
 }
 
+/// Key to request expansion of a ``TweetField`` into a complete object.
+/// Docs: https://developer.twitter.com/en/docs/twitter-api/expansions
+public enum TweetExpansion: String {
+    case author_id = "author_id"
+    case referenced_tweets_id = "referenced_tweets.id"
+    case in_reply_to_user_id = "in_reply_to_user_id"
+    case attachments_media_keys = "attachments.media_keys"
+    case attachments_poll_ids = "attachments.poll_ids"
+    case geo_place_id = "geo.place_id"
+    case entities_mentions_username = "entities.mentions.username"
+    case referenced_tweets_id_author_id = "referenced_tweets.id.author_id"
+}
+
 public func hydratedTweets(credentials: OAuthCredentials, ids: [Int]) async throws -> Void {
     var ids = ids
     if ids.count >= 100 {
