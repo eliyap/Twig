28cf7540439c189df28a975aa6466353559a4aa8
Ipsky <yap@usc.edu>
2021-10-23 14:49:03 -0700

    add expansion enum

--- Sources/Twig/Hydrate.swift
+++ Sources/Twig/Hydrate.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
+/// The additional information we may obtain for a Tweet Object.
+/// Docs: https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/tweet
+public enum TweetExpansion: String {
+    case attachments
+    case author_id
+    case context_annotations
+    case conversation_id
+    case created_at
+    case entities
+    case geo
+    case in_reply_to_user_id
+    case lang
+    case non_public_metrics
+    case organic_metrics
+    case possibly_sensitive
+    case promoted_metrics
+    case public_metrics
+    case referenced_tweets
+    case reply_settings
+    case source
+    case withheld
+}
+
 public func hydratedTweets(credentials: OAuthCredentials, ids: [Int]) async throws -> Void {
     var ids = ids
     if ids.count >= 100 {
