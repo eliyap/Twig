b50c33c2d2dca913aac7dfbb7e9004d15e8f1858
Ipsky <yap@usc.edu>
2021-10-24 11:26:16 -0700

    dev crash in failable

--- Sources/Twig/Failable.swift
+++ Sources/Twig/Failable.swift
@@ -10,11 +10,19 @@ import Foundation
 /// Stores a failure as `nil` instead of `throw`-ing.
 /// This allows us to salvage some elements in an array containing malformed objects.
 /// Source: https://stackoverflow.com/a/46369152/12395667
+/// If the value is malformed, cause a dev crash, but return nil in production.
 struct Failable<T: Decodable>: Decodable {
     let item: T?
     init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         self.item = try? container.decode(T.self)
+        
+        #if DEBUG
+        if item == nil {
+            /// Intentionally crash to reveal error.
+            _ = try! container.decode(T.self)
+        }
+        #endif
     }
 }
 
