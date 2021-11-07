6a907327f810310761c5e64c775996d85ea1d1c4
Ipsky <yap@usc.edu>
2021-11-07 13:13:49 -0800

    fix auth ordering

--- Sources/Twig/Following.swift
+++ Sources/Twig/Following.swift
@@ -7,15 +7,21 @@ public func requestFollowing(credentials: OAuthCredentials) async throws -> Void
     print(json)
 }
 
+/// Docs: https://developer.twitter.com/en/docs/twitter-api/users/follows/api-reference/get-users-id-following
 internal func follwingRequest(credentials: OAuthCredentials) -> URLRequest {
+    let additional = ["max_results": "10"]
+//    let additional: [String: String] = [:]
     /// Formulate request.
-    let followingURL = "https://api.twitter.com/2/users/\(credentials.user_id)/following"
+    var followingURL = "https://api.twitter.com/2/users/\(credentials.user_id)/following"
+    
+    /// - Note: Query string authorization did not work. Use header instead.
+    let parameters = signedParameters(method: .GET, url: followingURL, credentials: credentials, including: additional)
+    
+    followingURL.append(contentsOf: "?\(additional.parameterString())")
+    
     let url = URL(string: followingURL)!
     var request = URLRequest(url: url)
     request.httpMethod = HTTPMethod.GET.rawValue
-
-    /// - Note: Query string authorization did not work. Use header instead.
-    let parameters = signedParameters(method: .GET, url: followingURL, credentials: credentials)
     request.setValue("OAuth \(parameters.headerString())", forHTTPHeaderField: "authorization")
     
     return request
