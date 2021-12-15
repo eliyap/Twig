fd2508e2fd7684d3b9cdbc55f2c2264e6320a61d
Ipsky <yap@usc.edu>
2021-12-14 17:30:56 -0800

    flesh out custom struct

--- Sources/Twig/Parameters.swift
+++ Sources/Twig/Parameters.swift
@@ -10,16 +10,42 @@ import Foundation
 /// Set of parameters to be attached to Twitter API Requests.
 internal struct RequestParameters {
     /// Parameters that should be percent encoded.
-    var encodable: [String?]
+    public var encodable: [String: String?]
     
     /// Parameters that should **not** be percent encoded.
-    var nonEncodable: [String?]
+    public var nonEncodable: [String: String?]
     
-    init(
-        encodable: [String?] = [],
-        nonEncodable: [String?] = []
+    public init(
+        encodable: [String: String?] = [:],
+        nonEncodable: [String: String?] = [:]
     ) {
         self.encodable = encodable
         self.nonEncodable = nonEncodable
+    }
+    
+    internal static let Discard: (String, String) -> String = { (x, _) in
+        Swift.debugPrint("[WARNING], duplicate key: \(x)")
+        return x
+    }
+    
+    public mutating func merge(_ other: Self) {
+        encodable.merge(other.encodable) { (x, _) in x }
+        nonEncodable.merge(other.nonEncodable) { (x, _) in x }
+    }
+    
+    public static let OAuth: Self = .init(encodable: [
+        "oauth_consumer_key": Keys.consumer,
+        "oauth_nonce": nonce(),
+        "oauth_signature_method": "HMAC-SHA1",
+        "oauth_timestamp": "\(Int(Date().timeIntervalSince1970))",
+        "oauth_version": "1.0",
+    ])
+    
+    public func queryString() -> String {
+        encodable.compacted.unsafePercentEncoded()
+            .merging(nonEncodable.compacted, uniquingKeysWith: Self.Discard)
+            .keySorted()
+            .parameterString()
+            .addingPercentEncoding(withAllowedCharacters: .twitter)!
     }
 }
