fac78cd6bf6727f7ad62d42a72adaf3554e84d27
eliyap <yap@usc.edu>
2022-01-07 11:40:31 +0800

    add blackbox dependency and local logger

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
             url: "https://github.com/apple/swift-collections.git",
             .upToNextMajor(from: "1.0.0") // or `.upToNextMinor
         ),
+        .package(name: "BlackBox", path: "../Blackbox")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
