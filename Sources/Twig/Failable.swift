208f4a814815c64ce48ffa534bf76b7dd60a2366
eliyap <yap@usc.edu>
2022-01-07 19:48:02 +0800

    remove unused func

--- Sources/Twig/Failable.swift
+++ Sources/Twig/Failable.swift
@@ -25,24 +25,3 @@ struct Failable<T: Decodable>: Decodable {
         #endif
     }
 }
-
-/// Decode JSON array.
-/// If there are malformed values in the array,
-/// salvage the good ones, and cause a dev crash for the bad ones.
-internal func decodeFailableArray<T: Decodable>(from data: Data) throws -> [T] {
-    /// Attempt to decode each item, replacing failures with `nil`.
-    let failables = try JSONDecoder().decode([Failable<T>].self, from: data)
-    
-    /// Filter out `nil` values in production.
-    return failables.compactMap { (failable) -> T? in
-        if let item = failable.item {
-            return item
-        } else {
-            #if DEBUG
-            /// Intentionally crash to reveal error.
-            _ = try! JSONDecoder().decode([T].self, from: data)
-            #endif
-            return nil
-        }
-    }
-}
