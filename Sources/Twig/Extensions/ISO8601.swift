06601481a96cc392ba0e562294c5a0a5dde30728
eliyap <yap@usc.edu>
2022-01-07 19:57:11 +0800

    define format

--- Sources/Twig/Extensions/ISO8601.swift
+++ Sources/Twig/Extensions/ISO8601.swift
@@ -28,6 +28,18 @@ extension DateFormatter {
         formatter.locale = Locale(identifier: "en_US_POSIX")
         return formatter
     }()
+    
+    /// Docs:
+    /// https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/tweet
+    /// > Wed Oct 10 20:19:24 +0000 2018
+    static let v1dot1Format: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEE MMM dd HH:mm:ss Z yyyy"
+        formatter.calendar = Calendar(identifier: .iso8601)
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        return formatter
+    }()
 }
 
 extension Date {
