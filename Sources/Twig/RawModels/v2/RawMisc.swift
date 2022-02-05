d5a82b185d3a9d99dbb95b69f526d2a32299be63
eliyap <yap@usc.edu>
2022-02-04 19:32:37 -0800

    roughly describe v1.1 media objects

--- Sources/Twig/RawModels/v2/RawMisc.swift
+++ Sources/Twig/RawModels/v2/RawMisc.swift
@@ -16,5 +16,49 @@ public struct RawPlace: Codable, Sendable {
 }
 
 public struct RawExtendedEntities: Codable, Sendable {
-    /// Incomplete...
+    let media: [RawExtendedMedia]
+}
+
+public struct RawExtendedMedia: Codable, Sendable {
+    let id_str: String
+    
+    let media_url: String
+    let media_url_https: String
+    let url: String
+    let display_url: String
+    let expanded_url: String
+    
+    let type: RawIncludeMediaType
+    let video_info: RawVideoInfo
+}
+
+public struct RawMediaSizes: Codable, Sendable {
+    let thumb: RawMediaSize
+    let small: RawMediaSize
+    let medium: RawMediaSize
+    let large: RawMediaSize
+}
+
+public struct RawMediaSize: Codable, Sendable {
+    let w: Int
+    let h: Int
+    let resize: RawResize
+}
+
+public enum RawResize: String, Codable, Hashable, Sendable {
+    case fit, crop
+}
+
+public struct RawVideoInfo: Codable, Sendable {
+    let aspect_ratio: [Int]
+    let duration_millis: Int
+    let variants: [RawMediaVariant]
+}
+
+public struct RawMediaVariant: Codable, Sendable {
+    /// - Note: Bitrate is not present on formats such as `m3u8`.
+    ///         https://developer.twitter.com/en/docs/twitter-api/v1/data-dictionary/object-model/extended-entities#tweet-video
+    let bitrate: Int?
+    let content_type: RawIncludeContentType
+    let url: String
 }
